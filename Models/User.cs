f6dbc42630d3e3824bc3ef6ba62defce88645648
gerardo0579 <gerardo0579@hotmail.com>
2024-04-09 19:21:29 -0600

    Add first version for function app project

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+namespace HC.Function
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string CreatedAt { get; set; }
+        public string UpdatedAt { get; set; }
+    }
+}
